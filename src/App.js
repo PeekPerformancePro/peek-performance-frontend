fcb599fac995f91c9cbde82c419d05f41bca25b2
PeekPerformancePro <chrisjamesonpeek@gmail.com>
2025-03-25 07:40:19 -0400

    Fix: reinstall react-scripts and update package-lock..json

--- src/App.js
+++ src/App.js
@@ -8,12 +8,6 @@ function App() {
         <p>Welcome to your 24/7 Shelftalker platform.</p>
         </h1>Increase sales with our "Silent Salesperson" solution.</>
         </div>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
 }
 
 export default App;
